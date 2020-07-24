f68ccae82439177fc6b8c0aa648c87afbefdd578
Fredrik Rubensson <fredrik@eldfluga.se>
2020-07-24 21:38:53 +0200

    createing map in a function

--- adana.js
+++ adana.js
@@ -8,21 +8,28 @@ let markers = [
     }
 ]
 
-let map = L.map('adanamap').setView([59.3346, 18.066], 13);
-let anotherLayer = 'http://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+let createMap = () => {
+    let map = L.map('adanamap').setView([59.3346, 18.066], 13);
+    let anotherLayer = 'http://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
 
-let attribution = () => {
-    let osmAttribution = '&copy; '
-        + '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        + ' contributors'
-    let iconAttribution = 'Icons made by '
-        + '<a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>'
-        + ' from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>'
+    let attribution = () => {
+        let osmAttribution = '&copy; '
+            + '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            + ' contributors'
+        let iconAttribution = 'Icons made by '
+            + '<a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>'
+            + ' from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>'
 
-    return osmAttribution + ' | ' + iconAttribution
+        return osmAttribution + ' | ' + iconAttribution
+    }
+
+    L.tileLayer(anotherLayer, { attribution: attribution() }).addTo(map);
+
+    return map
 }
 
-L.tileLayer(anotherLayer, { attribution: attribution() }).addTo(map);
+let map = createMap()
+
 
 for(let m of markers) {
     let popupOptions = { className: 'markerPopup' }
